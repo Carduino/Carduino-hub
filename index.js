867f078e0e4d24f93d336715d933b9fb8981eab2
Vincent Dauliac <vincent.dauliac@gmail.com>
2016-04-24 13:14:01 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,91 @@
+// Node modules
+var util = require('util'),
+	SerialPort = require('serialport').SerialPort,
+	xbee_api = require('xbee-api'),
+	socketIO = require('socket.io-client'),
+	jwt = require('jsonwebtoken');
+
+
+
+// Config variables
+var hubName = 'Hub 1',
+	serverAddress = 'dauliac.fr:3000';
+
+
+
+// General variables
+var authToken,
+	socket,
+	sensorDatas = [],
+	hub = {
+		name: hubName,
+		children: []
+	};
+
+
+
+// Create a jwt with HMAC using SHA-256 hash algorithm
+authToken = jwt.sign({
+	name: hubName,
+	role: 'hub'
+}, 'pwd', {
+	algorithm: 'HS256',
+	issuer: 'Carduino-server'
+});
+
+
+
+// XBee
+var xbeeAPI = new xbee_api.XBeeAPI({
+	api_mode: 2
+});
+
+var serialport = new SerialPort("/dev/ttyAMA0", {
+	baudrate: 9600,
+	parser: xbeeAPI.rawParser()
+});
+
+
+
+// Socket.io Websocket connexion init
+socket = socketIO('ws://' + serverAddress);
+
+socket.on('connect', function() {
+	socket.emit('authenticate', {
+		token: authToken
+	});
+});
+
+socket.on('authenticated', function() {
+	// Emit the datas refering to the hub and the Sensors connected
+	socket.emit('newHub', hub);
+
+	// All frames parsed by the XBee will be catched here
+	xbeeAPI.on("frame_object", function(frame) {
+		if (frame.data !== undefined)
+			console.log(frame.data.toString('utf8'));
+		socket.emit('sensorsDatas', frame.data.toString('utf8'));
+	});
+
+	/*
+
+		var sensor = {
+			name: ...
+		};
+		hub.children.push(sensor)
+		// Emit the connexion of a new Sensors
+		socket.emit('newSensor', sensor);
+
+
+
+		// Emit the lost of a Sensor
+		socket.emit('sensorLost', sensorName);
+
+
+
+		// Emit every 5 seconds the datas of each sensor connected
+		var interval = setInterval(function() {
+			socket.emit('sensorsDatas', sensorsDatas);
+		}, 1000);
+	*/
+});
